604f5db2a78927b9782f269c0cee603fe501dc2d
Pavan699 <pavannakate03@gmail.com>
2021-10-22 06:27:17 -0700

    Welcome message to Product Review

---
+++ ProductRatingProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProductRatingProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Product Rating");
+        }
+    }
+}
